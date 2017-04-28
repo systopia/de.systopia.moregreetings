d1f349b62170e824b390d7a5cb4af14dd1554ba0
systopia <endres@systopia.de>
2017-04-28 07:35:39 +0200

    added file headers

--- moregreetings.php
+++ moregreetings.php
@@ -1,4 +1,19 @@
 <?php
+/*-------------------------------------------------------+
+| SYSTOPIA - MORE GREETINGS EXTENSION                    |
+| Copyright (C) 2017 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
+|         P. Batroff (batroff@systopia.de)               |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
 
 require_once 'moregreetings.civix.php';
 
