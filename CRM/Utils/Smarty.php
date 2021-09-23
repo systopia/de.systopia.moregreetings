900a3362e976966eae0aeb6e13d681fe6c3eaf8d
B. Endres <endres@systopia.de>
2021-09-23 19:16:51 +0200

    [#23] added tokens_have_min_length and token_extract functions

--- CRM/Utils/Smarty.php
+++ CRM/Utils/Smarty.php
@@ -25,9 +25,11 @@ class CRM_Utils_Smarty {
    * @param Smarty $smarty
    */
   public static function registerCustomFunctions($smarty) {
-    $smarty->register_modifier('mg_startswith', array('CRM_Utils_Smarty', 'startswith'));
-    $smarty->register_modifier('mg_endswith',   array('CRM_Utils_Smarty', 'endswith'));
-    $smarty->register_modifier('mg_contains',   array('CRM_Utils_Smarty', 'contains'));
+    $smarty->register_modifier('mg_startswith',            ['CRM_Utils_Smarty', 'startswith']);
+    $smarty->register_modifier('mg_endswith',              ['CRM_Utils_Smarty', 'endswith']);
+    $smarty->register_modifier('mg_contains',              ['CRM_Utils_Smarty', 'contains']);
+    $smarty->register_modifier('tokens_have_min_length',   ['CRM_Utils_Smarty', 'tokens_have_min_length']);
+    $smarty->register_modifier('token_extract',            ['CRM_Utils_Smarty', 'token_extract']);
     $smarty->register_modifier('lcfirst',    'lcfirst');
     $smarty->register_modifier('ucfirst',    'ucfirst');
   }
@@ -63,5 +65,68 @@ class CRM_Utils_Smarty {
         return strlen($strstr) > 0;
       }
     }
+  }
+
+  /**
+   * Split the given string by the given $split_string into individual tokens,
+   *   and then check if all these tokens have a minimum length of $length
+   * If $token_indices is present, apply only to the tokens with the given indices
+   *
+   * @param $string string
+   *    the input string
+   * @param $split_string string
+   *    the term used to split the string
+   * @param $length integer
+   *    the minimum length to be tested for
+   * @param $token_indices array|string
+   *    only check the given indices instead of all - provided they exist.
+   *
+   * Smarty usage: {if $contact.first_name|tokens_have_min_length:' ':2}
+   *     is true, if no single characters are used in the first_name
+   */
+  public static function tokens_have_min_length($string, $split_string, $length, $token_indices = null) {
+    $length = (int) $length;
+    $tokens = explode($split_string, $string);
+
+    // default for token indices is all
+    if ($token_indices === null) {
+      $token_indices = array_keys($tokens);
+    }
+
+    // if somebody just passed a single one or a comma separated string, turn that into an array
+    if (!is_array($token_indices)) {
+      $token_indices = explode(',', $token_indices);
+    }
+
+    // now check all of them
+    foreach ($tokens as $index => $token) {
+      if (isset($token_indices[$index])) {
+        if (strlen($token) < $length) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Split the given string by the given $split_string into individual tokens,
+   *   and extract the token with the given index
+   *
+   * @param $string string
+   *    the input string
+   * @param $split_string string
+   *    the term used to split the string
+   * @param $index integer
+   *    the index of the token to return
+   *
+   * Smarty usage: {$contact.first_name|token_extract:' ':1}
+   *    will return the 'van' if the first_name is "Herbert van Halen"
+   */
+  public static function token_extract($string, $split_string, $index = 0) {
+    $index = (int) $index;
+    $tokens = explode($split_string, $string);
+    return $tokens[$index] ?? '';
   }
 }
